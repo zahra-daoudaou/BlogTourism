13bd6ff489513d2b55a34c5408d07e8b86ae8330
Zahra <87523308+zahra-daoudaou@users.noreply.github.com>
2024-02-23 09:33:32 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,5 @@ Route::get('/post_details/{id}',[homeController::class,'post_details']);
 Route::get('/create_post',[homeController::class,'create_post']);
 Route::post('/user_post',[homeController::class,'user_post']);
 
-Route::resource('taches',tachesController::class);
+Route::resource('taches',tachController::class);
 
