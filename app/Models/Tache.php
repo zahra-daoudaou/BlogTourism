a2774781f3c389e831dde062b0a9b65390c0996c
Zahra <87523308+zahra-daoudaou@users.noreply.github.com>
2024-02-23 09:05:24 +0100

    Update Tache.php

--- app/Models/Tache.php
+++ app/Models/Tache.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tache extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'titre',
+        'description',
+        'etat'
+    ];
 }
