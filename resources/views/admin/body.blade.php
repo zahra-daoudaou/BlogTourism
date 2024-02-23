58b9f745ad0243543911b980ba5fc895f959ec5a
Zahra <87523308+zahra-daoudaou@users.noreply.github.com>
2024-02-23 09:01:24 +0100

    Update body.blade.php

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -4,5 +4,32 @@
       <h2 class="h5 no-margin-bottom">Admin Page</h2>
     </div>
   </div>
-        
+        <h3>Ajouter une tache</h3>
+
+    <form method="post" action="{{route('taches.store')}}">
+        @csrf
+        @method('post')
+        <label for="titre">Titre:</label>
+        <input type="text" name="titre">
+        <label for="description">Description:</label>
+        <input type="text" name="description">
+
+        <input type="radio" id="termine" name="etat" value="termine">
+        <label for="Terminé">Terminé</label>
+        <input type="radio" id="non_termine" name="etat" value="non_termine">
+        <label for="Non terminé">Non terminé</label>
+
+        <div>
+            <input type="submit" value="Ajouter la tache">
+        </div>
+
+        <div>
+            @if($errors->all())
+            <ul>
+                @foreach($errors->all() as $error)
+                <li> {{$error}} </li>
+                @endforeach
+            </ul>
+            @endif
+        </div>
 </div> 
