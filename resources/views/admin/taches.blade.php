9c21a348cdc4b16461003c12945f4380bc4b936b
Zahra <87523308+zahra-daoudaou@users.noreply.github.com>
2024-02-23 09:21:14 +0100

    Update taches.blade.php

--- resources/views/admin/taches.blade.php
+++ resources/views/admin/taches.blade.php
@@ -39,7 +39,19 @@
 
       <div class="page-content">
 
-        <h1 class="post_title" >POSTS</h1>
+        <h1 class="post_title" >My tasks</h1>
+
+          <!--<input type="search" name="search" id="search" value="{{request('search')}}">
+
+    <ul>
+        @forelse($taches as $tache)
+        <li>{{$tache->titre}}</li>
+        <li>{{$tache->description}}</li>
+        <li>{{$tache->etat}}</li>
+        @empty
+        <li>Tache non trouvable!</li>
+        @endforelse
+    </ul>-->
 
         <div>
             <form method="get" action="{{ url('show_post') }}">
@@ -61,40 +73,67 @@
         </div>
 
         </div>
-        <!-- ----------------- -->
-        <table class="table_des" rules="all">
+<form action="{{ route('taches.index') }}" method="get">
+        <label for="filter">Filtrer par état :</label>
+
+        <select name="filter" id="filter">
+            <option value="">Tous</option>
+            <option value="terminé" {{ $filter === 'termine' ? 'selected' : '' }}>Terminées</option>
+            <option value="not_completed" {{ $filter === 'non_termine' ? 'selected' : '' }}>Non terminées</option>
+        </select>
+
+        <button type="submit">Filtrer</button>
+    </form>
+
+    <div>
+        @if(session()->has('success'))
+        <div>
+            {{session('success')}}
+        </div>
+        @endif
+    </div>
+
+       <!-- ----------------- -->
+
+
+        <table class="table_des" rules="all" rules="all" >
         <tr th_des>
             <th>Id</th>
-            <th>Title</th>
+            <th>Titre</th>
             <th>Description</th>
-            <th>Image</th>
+            <th>état</th>
             <th>Edit</th>
             <th>Delete</th>
         </tr>
-        @forelse($post as $post)
-    <tr>
-        <td> {{$post->id}} </td>
-        <td>{{$post->title}}</td>
-        <td>{{$post->description}}</td>
+        @forelse($taches as $tache)
+    <tr th_des>
+        <td> {{$tache->id}} </td>
+        <td>{{$tache->titre}}</td>
+        <td>{{$tache->description}}</td>
+        <td>{{$tache->etat}}</td>
         <td>
-            <img class="img_drg" src="images/{{$post->image}}">
+        <a href="{{ route('taches.edit', $tache->id) }}">modifier</a>
         </td>
         <td>
-        <a href="">modifier</a>
-        </td>
-        <td>
-        <form method="post" action="}}">
+        <form method="post" action="{{route('taches.destroy',$tache->id)}}">
             @csrf
             @method('delete')
             <input type="submit" value="supprimer">
         </form>
         </td>
         @empty
-        <td>Post not found!</td>
+        <td>tache non trouvable!</td>
         </tr>
     @endforelse
     </table>
-    <!-- ----------------- -->
+    <form action="{{route('taches.create')}}">
+        <input type="submit" value="Ajouter une tache">
+    </form>
+
+    {{ $taches->links() }}
+
+        <!-- -------------------------------- -->
+
       </div>
 
     <!-- Footer Navigation-->
