ec61db1a3dc836700235216db676706460e3a22f
Zahra <87523308+zahra-daoudaou@users.noreply.github.com>
2024-02-23 09:06:36 +0100

    Update body.blade.php

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -4,32 +4,74 @@
       <h2 class="h5 no-margin-bottom">Admin Page</h2>
     </div>
   </div>
-        <h3>Ajouter une tache</h3>
+        <!--<input type="search" name="search" id="search" value="{{request('search')}}">
 
-    <form method="post" action="{{route('taches.store')}}">
-        @csrf
-        @method('post')
-        <label for="titre">Titre:</label>
-        <input type="text" name="titre">
-        <label for="description">Description:</label>
-        <input type="text" name="description">
+    <ul>
+        @forelse($taches as $tache)
+        <li>{{$tache->titre}}</li>
+        <li>{{$tache->description}}</li>
+        <li>{{$tache->etat}}</li>
+        @empty
+        <li>Tache non trouvable!</li>
+        @endforelse
+    </ul>-->
 
-        <input type="radio" id="termine" name="etat" value="termine">
-        <label for="Terminé">Terminé</label>
-        <input type="radio" id="non_termine" name="etat" value="non_termine">
-        <label for="Non terminé">Non terminé</label>
+    <form action="{{ route('taches.index') }}" method="get">
 
+        <label for="filter">Filtrer par état :</label>
+
+        <select name="filter" id="filter">
+            <option value="">Tous</option>
+            <option value="terminé" {{ $filter === 'termine' ? 'selected' : '' }}>Terminées</option>
+            <option value="not_completed" {{ $filter === 'non_termine' ? 'selected' : '' }}>Non terminées</option>
+        </select>
+
+        <button type="submit">Filtrer</button>
+    </form>
+
+    <div>
+        @if(session()->has('success'))
         <div>
-            <input type="submit" value="Ajouter la tache">
+            {{session('success')}}
         </div>
+        @endif
+    </div>
 
-        <div>
-            @if($errors->all())
-            <ul>
-                @foreach($errors->all() as $error)
-                <li> {{$error}} </li>
-                @endforeach
-            </ul>
-            @endif
-        </div>
+    <h3>List des Taches</h3>
+
+    <table rules="all" border="1" >
+        <tr>
+            <th>Id</th>
+            <th>Titre</th>
+            <th>Description</th>
+            <th>état</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        @forelse($taches as $tache)
+    <tr>
+        <td> {{$tache->id}} </td>
+        <td>{{$tache->titre}}</td>
+        <td>{{$tache->description}}</td>
+        <td>{{$tache->etat}}</td>
+        <td>
+        <a href="{{ route('taches.edit', $tache->id) }}">modifier</a>
+        </td>
+        <td>
+        <form method="post" action="{{route('taches.destroy',$tache->id)}}">
+            @csrf
+            @method('delete')
+            <input type="submit" value="supprimer">
+        </form>
+        </td>
+        @empty
+        <td>tache non trouvable!</td>
+        </tr>
+    @endforelse
+    </table>
+    <form action="{{route('taches.create')}}">
+        <input type="submit" value="Ajouter une tache">
+    </form>
+
+    {{ $taches->links() }}
 </div> 
